bd6adf197f5ab5694a1afd19c4fe5621be6ecca1
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-10-12 00:15:54 -0500

    updated

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,7 +4,7 @@ export default class PostForm extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Add Post</h1>
             </div>
         )
     }
