6dc7d50ddedbce513eaca537ec20c6221895ec1e
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-10-17 23:30:49 -0500

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Posts from './components/Posts'
+import {Button} from 'bootstrap'
 
 function App() {
   return (
     <div className="App">
       {/* <div> Test</div> */}
       <Posts />
+      <Button className="btn btn-primary">sindhu</Button>
     </div>
   );
 }
